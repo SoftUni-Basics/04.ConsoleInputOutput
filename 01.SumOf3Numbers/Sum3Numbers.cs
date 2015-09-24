23a7f7b0230f7c6c782be357483cf35c1e7cef89
GogoK <georgiakanchev@gmail.com>
2015-09-25 00:29:30 +0300

    1-sum of three numbers

---
+++ 01.SumOf3Numbers/Sum3Numbers.cs
@@ -0,0 +1,21 @@
+﻿/*Write a program that reads 3 real numbers from the console and prints their sum.*/
+
+using System;
+
+class Sum3Numbers
+{
+    static void Main()
+    {
+        Console.Write("Enter value A = ");
+        double a = double.Parse(Console.ReadLine());
+
+        Console.Write("Enter value B = ");
+        double b = double.Parse(Console.ReadLine());
+
+        Console.Write("Enter value C = ");
+        double c = double.Parse(Console.ReadLine());
+
+        double sum = a + b + c;
+        Console.WriteLine("Sum[a,b,c] = {0}", sum);
+    }
+}
