93317b86896ea56f48ea9950d6f554797e79b759
GogoK <georgiakanchev@gmail.com>
2015-09-25 00:41:22 +0300

    7- sum of five numbers

---
+++ 07.SumOfFiveNumbers/SumOfFiveNumbers.cs
@@ -0,0 +1,18 @@
+﻿/*  Write a program that enters 5 numbers (given in a single line, separated by a space), calculates and prints their sum.
+ */
+
+using System;
+using System.Linq;
+
+class SumOfFiveNumbers
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter Five numbers separated by a space: ");
+        decimal[] sumArray = Console.ReadLine().Split(new char[] { ' ' }, 
+            StringSplitOptions.RemoveEmptyEntries).Select(decimal.Parse).ToArray();
+        
+        Console.WriteLine("Sum = {0}", sumArray.Sum());
+        
+    }
+}
